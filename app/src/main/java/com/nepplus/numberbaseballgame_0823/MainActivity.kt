d679fc45b15bbd04e4e00b6039acacdca029741a
송병섭 <tumtum@naver.com>
2021-08-23 15:38:31 +0900

    문제를 임시로 471로 고정하여 제출 + 환영 메시지 출력

--- app/src/main/java/com/nepplus/numberbaseballgame_0823/MainActivity.kt
+++ app/src/main/java/com/nepplus/numberbaseballgame_0823/MainActivity.kt
@@ -12,12 +12,15 @@ class MainActivity : AppCompatActivity() {
 
     lateinit var mAdapter:MessageAdapter
 
+    // 세자리 문제 숫자를 저장하기 위한 ArrayList
+    val mQuestionNumbers = ArrayList<Int>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        mMessageList.add(MessageData("안녕하세요","CPU"))
-        mMessageList.add(MessageData("반갑습니다","USER"))
+        makeQuestionNumbers() // 세 자리 랜덤 문자 만들기
+
 
         mAdapter = MessageAdapter(this, R.layout.message_list_item, mMessageList)
 
@@ -37,4 +40,17 @@ class MainActivity : AppCompatActivity() {
             messageListView.smoothScrollToPosition(mMessageList.lastIndex)
         }
     }
+
+    fun makeQuestionNumbers() {
+        mQuestionNumbers.add(4)
+        mQuestionNumbers.add(7)
+        mQuestionNumbers.add(1)
+        // 환영 메시지를 채팅창에 띄우자
+        // 메시지 리스트에 띄워줄 만들 데이터를 추가하자
+
+        mMessageList.add(MessageData("어서오세요.","CPU"))
+        mMessageList.add(MessageData("숫자 야구 게임입니다.","CPU"))
+        mMessageList.add(MessageData("세자리 숫자를 맞춰주세요.","CPU"))
+    }
+
 }
