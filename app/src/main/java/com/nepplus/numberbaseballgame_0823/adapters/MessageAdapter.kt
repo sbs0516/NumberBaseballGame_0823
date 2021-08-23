66e40f2da74d9671ec0667078b024566b2e55f8b
송병섭 <tumtum@naver.com>
2021-08-23 14:55:37 +0900

    메시지 어댑터 뼈대 작업 - 다른 프로젝트에서 복사해오기

---
+++ app/src/main/java/com/nepplus/numberbaseballgame_0823/adapters/MessageAdapter.kt
@@ -0,0 +1,38 @@
+package com.nepplus.numberbaseballgame_0823.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.TextView
+import com.nepplus.numberbaseballgame_0823.R
+import com.nepplus.numberbaseballgame_0823.datas.MessageData
+
+class MessageAdapter(
+    val mContext: Context,
+    val resId: Int,
+    val mList: ArrayList<MessageData>) : ArrayAdapter<MessageData>(mContext, resId, mList) {
+
+    val mInflater = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var tempRow = convertView
+
+        if (tempRow == null) {
+            tempRow = mInflater.inflate(R.layout.message_list_item, null)
+        }
+
+        val row = tempRow!!
+
+
+        val data = mList[position]
+
+
+
+        return row
+    }
+
+
+}
