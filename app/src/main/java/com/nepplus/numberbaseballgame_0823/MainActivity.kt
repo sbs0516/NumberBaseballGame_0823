4a8ecc43a435dd0af7577c9c76b5be3085c64f8b
송병섭 <tumtum@naver.com>
2021-08-23 16:06:22 +0900

    ?S ?B 판단하여 컴퓨터가 답장하는 기능까지 완성

--- app/src/main/java/com/nepplus/numberbaseballgame_0823/MainActivity.kt
+++ app/src/main/java/com/nepplus/numberbaseballgame_0823/MainActivity.kt
@@ -38,6 +38,9 @@ class MainActivity : AppCompatActivity() {
             numberEdt.setText("")
             // 문구를 비우고 나면, 리스트뷰를 맨 마지막 데이터로 내리기
             messageListView.smoothScrollToPosition(mMessageList.lastIndex)
+
+            // 컴퓨터가 ?S ?B 을 판단하여 메시지 추가(답장)
+            checkAnswer(inputNumStr.toInt())
         }
     }
 
@@ -51,6 +54,38 @@ class MainActivity : AppCompatActivity() {
         mMessageList.add(MessageData("어서오세요.","CPU"))
         mMessageList.add(MessageData("숫자 야구 게임입니다.","CPU"))
         mMessageList.add(MessageData("세자리 숫자를 맞춰주세요.","CPU"))
+    }
+
+    fun checkAnswer(inputNum: Int) {
+        // 유저가 입력한 숫자가 ?S ?B 인지 판단하는 함수
+
+        // 사람이 입력한 숫자를 각 자리별로 나눠서 목록에 대입
+        val userInputNumArr = ArrayList<Int>()
+        userInputNumArr.add(inputNum / 100)
+        userInputNumArr.add((inputNum / 10) % 10)
+        userInputNumArr.add(inputNum % 10)
+
+        var strikeCount = 0
+        var ballCount = 0
+
+        for(i in 0..2) {
+            for(j in 0..2) {
+                // 내가 입력한 숫자 i 번째와 컴퓨터가 낸 숫자 j 번째가 같은 숫자인지?
+                if(userInputNumArr[i] == mQuestionNumbers[j]) {
+                    // 같은 숫자를 찾음
+                    // 위치도 같은지 여부 확인
+                    if(i == j) {
+                        strikeCount++
+                    } else {
+                        ballCount++
+                    }
+                }
+            }
+        }
+        // ?S ?B 인지를 컴퓨터가 말하는 것으로 처리
+        mMessageList.add(MessageData("${strikeCount}S ${ballCount}B 입니다", "CPU"))
+        mAdapter.notifyDataSetChanged()
+        messageListView.smoothScrollToPosition(mMessageList.lastIndex)
     }
 
 }
